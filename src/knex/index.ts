94560367929e42a602cea77a8faf2b89e9ec3d90
yashmunjal <yashmu45@gmail.com>
2020-06-07 02:18:50 +0530

    addition

--- src/knex/index.ts
+++
@@ -1,8 +0,0 @@
-import knex from 'knex';
-import {config as dbConfig} from './config';
-
-const env = process.env.NODE_ENV || 'development';
-
-const config = env === 'development' ? dbConfig.development : dbConfig.production;
-
-export default knex(config);
